05780e9278d1c30bef9c690a39a576e202637e4f
BearOnABicycle <n.farrill904@mgail.com>
2023-10-02 18:10:10 -0400

    Create errors.js
    Demonstrate multiple ways errors and bugs can occure in JS.

---
+++ javascript-practice/errors.js
@@ -0,0 +1,14 @@
+//create a bug that outputs an unexpected result
+// function addNums(a, b){
+//   console.log(a + b);
+// }
+// addNums("1", 2);// output: 12; this is due to the concatination properties of strings
+//
+// console.log("Still Running");// Shows that a but has occured but the program still finishes running
+//
+// console.log(a + b);// ouputs an error because a is not defined as a variable
+// console.log("Still Running");//does not run due to an error being thrown earlier in the program
+
+// console.log("hello);// creates a syntax error due to the string missing an ending quotation 
+
+(5).pop; // creates a type error because pop is a method of arrays not of integers
