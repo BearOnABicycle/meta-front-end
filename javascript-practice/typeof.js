31ae9fbaee238faab2728f704121a780b1c4dda6
BearOnABicycle <n.farrill904@mgail.com>
2023-10-02 17:43:15 -0400

    Create typeof.js
    Demonstrate the use of the typeof operator.

---
+++ javascript-practice/typeof.js
@@ -0,0 +1,11 @@
+//demonstrate the use of the typeof() operator
+
+// var test = typeof(false);
+// var test = typeof(99);
+// var test = typeof(0.99);
+// var test = typeof("Hello");
+// var test = typeof([1, 2, 3]);
+var test = typeof({ firstProperty: 1});
+// var test = typeof(function abc(){ console.log("abc"); })
+
+console.log(test);
