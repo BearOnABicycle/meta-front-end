f9ab70de5b04ce52a068a3894e1529d2e4aeda7a
BearOnABicycle <n.farrill904@mgail.com>
2023-10-30 18:51:44 -0400

    Demonstrate the use if for of loops on objects
    Use the built in Object methods of Object.key, Object.values and
    Object.entries to iterate within an object.

--- javascript-practice/advanced-features/for-of-loops-and-objects.js
+++ javascript-practice/advanced-features/for-of-loops-and-objects.js
@@ -12,7 +12,7 @@
 //
 // Output: Uncaught TypeError: car is not iterable
   
-//objects arrays are iterable
+// Arrays can be iterated
 
 const colors = ['red', 'orange', 'yellow']
 
@@ -20,4 +20,58 @@ for (var color of colors) {
   console.log(color)
 }
 
+// There are built in methods that allow us to iterate arrays within objects
+//
+// Object.keys will receive an object as an array of it's parameters
+const car2 = {
+  speed: 200,
+  color: 'red'
+}
+console.log(Object.keys(car2))
+
+// object.keys will return the keys of an object
+const car3 = {
+  speed: 300,
+  color: 'yellow'
+}
+console.log(Object.values(car3))
+
+// Object.entries will return an array of both keys and values (an array of arrays)
+const car4 = {
+  speed: 200,
+  color: 'magenta'
+}
+console.log(Object.entries(car4))
+
+// With these built in methods you can loop over and objects keys and values
+
+var clothingItem = {
+  price: 50,
+  color: 'beige',
+  material: 'cotton',
+  season: 'autumn'
+}
+
+for(const key of Object.keys(clothingItem)) {
+  console.log(key, ':', clothingItem[key])
+}
+
+
+
+ function testBracketDynamicAccess() {
+  var dynamicKey;
+  if(Math.random() > 0.5) {
+    dynamicKey = "speed"
+  }else{
+    dynamicKey = "color"
+  }
   
+  var drone = {
+    speed: 15, 
+    color: 'orange'
+  }
+
+  console.log(drone[dynamicKey])
+}
+
+testBracketDynamicAccess()
