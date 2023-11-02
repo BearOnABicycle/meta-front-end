a4fc306bd48c20b83bbe19aa5d54be9dd0785733
BearOnABicycle <n.farrill904@mgail.com>
2023-11-02 17:20:49 -0400

    Complete array and object iteration assignment
    Complete asaignment by following the three steps. Iterate over
    an array, iterate over and object, iterate over an object and
    it's prototype.

--- javascript-practice/advanced-features/array-object-iteration-assignment.js
+++ javascript-practice/advanced-features/array-object-iteration-assignment.js
@@ -18,14 +18,19 @@ canJump: true
 
 const bird = Object.create(animal);
 
-bird.canFly = true;
+bird.canFly = true
 
-bird.hasFeathers = true;
+bird.hasFeathers = true
+
 function birdCan() {
-    for (var properties of bird.keys())
-        console.log(properties)
+    for (var [key, value] of Object.entries(bird))
+        console.log(`${key}: ${value}`)
 }
-
+birdCan()
 // Task 3
-
-
+function animalCan() {
+  for (prop in bird){
+    console.log(`${prop}: ${bird[prop]}`)
+  }
+}
+animalCan()
